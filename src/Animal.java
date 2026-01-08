459013f4366c1b332fd0aa253f3f81989074af3e
Your Name <loic andrianarivony>
2026-01-08 10:09:59 +0100

    feat(Animal): create a class Animal

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+
+    String nom;
+    int age;
+
+    Animal(String nom, int age) {
+        this.nom = nom;
+        this.age = age;
+    }
+
+    void makeSound() {
+        System.out.println("L'animal fait un son.");
+    }
+}
+
